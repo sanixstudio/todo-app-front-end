bf357a6c73fed4153411cca84b7692399f1cf3c5
Adnan Niaz <adnanniaz77@yahoo.com>
2020-08-19 23:48:43 -0700

    added 404 page and some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.scss";
 import Navbar from "./components/layout/Navbar";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { TodoList } from "./components/pages/TodoList";
 import { CreateTodo } from "./components/pages/CreateTodo";
 import { EditTodo } from "./components/pages/EditTodo";
+import { Page404 } from "./components/pages/Page404";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                     <Route exact path="/" component={TodoList} />
                     <Route path="/todo/add" component={CreateTodo} />
                     <Route path="/todo/edit/:id" component={EditTodo} />
+                    <Route path to="/404" component={Page404} />
                 </Switch>
             </BrowserRouter>
         </div>
---
+++ src/components/pages/Page404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Page404 = () => {
+    return (
+        <div className="container d-flex justify-content-center mt-5">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>Page Not found</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
